b354539d0790079aee9238e6def3f9b3e5baad85
DUMA land <othanhti12395@gmail.com>
2023-05-11 21:23:43 +0700

    brithdat mf

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import App from './App';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
