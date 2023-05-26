2ae25e7f8d1cb32c87a4b1fd902e7f719aca7e5f
DUMA land <othanhti12395@gmail.com>
2023-05-26 10:19:13 +0700

    e

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 import App from "./App";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
